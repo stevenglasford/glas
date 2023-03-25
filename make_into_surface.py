30bfbe64b5aae765f977eb46fa33b49857e0f922
Steven Glasford <stevenglasford@duck.com>
2023-03-25 12:53:55 -0500

    Begin to work on cutting of plane

---
+++ make_into_surface.py
@@ -0,0 +1,7 @@
+surf = addobject("Surface::Section", "Surface")
+surf.NSections = [(Gui.Selection.addSelection(\'little_table\',\'Part018\',\'Sketch021.Edge6\',-64.4843,185.095,0), "e"),
+		{Gui.Selection.addSelection(\'little_table\',\'Part018\',\'Sketch021.Edge5\',-62.7023,190.441,0), "e"),
+		(Gui.Selection.addSelection(\'little_table\',\'Part018\',\'Sketch021.Edge5\',-62.7023,190.441,0), "e",
+		(Gui.Selection.addSelection(\'little_table\',\'Part018\',\'Sketch021.Edge5\',-62.7023,190.441,0), "e"),
+		(Gui.Selection.addSelection(\'little_table\',\'Part018\',\'Sketch021.Edge5\',-62.7023,190.441,0), "e"),
+		(Gui.Selection.addSelection(\'little_table\',\'Part018\',\'Sketch021.Edge5\',-62.7023,190.441,0), "e")]
