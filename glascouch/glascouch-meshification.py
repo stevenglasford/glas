9d68ee4a7c230d75302eb0b317de9532a3e9c12e
Steven Glasford <stevenglasford@duck.com>
2023-05-15 11:20:50 -0500

    Add the couch basics of importing the couch into the gmsh python file

--- glascouch/glascouch-meshification.py
+++ glascouch/glascouch-meshification.py
@@ -1,6 +1,23 @@
 import stl
 import gmsh
-import sys
+import os
+import math
 
 gmsh.initialize()
 
+# import the couch STL file into python
+def createGeometryAndMesh():
+    gmsh.clear()
+    path = os.path.dirname(os.path.abspath(__file__))
+    gmsh.merge(os.path.join(path, os.pardir, 'couch.stl'))
+
+gmsh.model.geo.synchronize()
+
+gmsh.model.mesh.generate()
+
+gmsh.write("GFG.msh")
+
+if 'close' not in sys.argv:
+    gmsh.fltk.run()
+
+gmsh.finalize()
